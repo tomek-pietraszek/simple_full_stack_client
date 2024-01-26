ba60e5d604bf6ef50708875493314eb5902e74ef
Tomasz Pietraszek <tomekpietraszek@gmail.com>
2024-01-26 13:47:58 +0100

    problem with env variable solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Submit from './components/Submit'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Submit from "./components/Submit";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -30,9 +30,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
 
-      <Submit count={count}/>
+      <Submit count={count} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/apiCalls.js/countApi.js
+++ src/apiCalls.js/countApi.js
@@ -1,5 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
-export const sentCount = async(count) => {
-    axios.patch(`${import.meta.env.VITE_API}/counts/${import.meta.env.VITE_COUNT_ID}`, {count})
-}
+export const sentCount = async (count) => {
+  axios.patch(
+    `${import.meta.env.VITE_API}/counts/${import.meta.env.VITE_COUNT_ID}`,
+    { count }
+  );
+};
