3db0f7cc310955df0f907dbb189be73a3767c234
Tomasz Pietraszek <tomekpietraszek@gmail.com>
2024-01-30 17:21:10 +0100

    headers added to patch request

--- src/apiCalls.js/countApi.js
+++ src/apiCalls.js/countApi.js
@@ -1,28 +1,31 @@
 import axios from "axios";
 
-
 //Allow cookies to be sent do the browser. It sets a default configuration for Axios requests to include credentials when making cross-origin requests
 axios.defaults.withCredentials = true;
 
 export const sentCount = async (count) => {
   axios.patch(
     `${import.meta.env.VITE_API}/counts/${import.meta.env.VITE_COUNT_ID}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
     { count }
   );
 };
 
-
 export const getCountFromCookies = async (setCount) => {
   try {
-    const response = await axios.get(`${import.meta.env.VITE_API}/counts/count`)
+    const response = await axios.get(
+      `${import.meta.env.VITE_API}/counts/count`
+    );
 
-    if(response.data){
-      
-    
-    //convert count to number. It comes from json object, therefore it's a string
-     setCount( +response.data.count)
+    if (response.data) {
+      //convert count to number. It comes from json object, therefore it's a string
+      setCount(+response.data.count);
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
